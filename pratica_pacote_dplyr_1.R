f327f3fc0a3f361c381b7ae865a6394cdac57969
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-02-03 23:43:08 -0300

    Add files via upload

--- pratica_pacote_dplyr_1.R
+++ pratica_pacote_dplyr_1.R
@@ -15,4 +15,19 @@ dados %>%
   select(weight, Diet) %>%
   group_by(Diet) %>% # Sem o group_by é retornado a média total dos pesos
   summarise(mean(weight),
+            sd(weight),
+            n())
+
+dados %>%
+  select(weight, Time) %>%
+  group_by(Time) %>% 
+  summarise(mean(weight),
+            sd(weight),
+            n())
+
+dados %>%
+  select(weight, Chick) %>%
+  group_by(Chick) %>% 
+  summarise(mean(weight),
+            sd(weight),
             n())
